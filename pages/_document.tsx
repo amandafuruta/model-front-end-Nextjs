94ef96f945f5bbc1ce28aac7482667ebff16336a
Amandaft07 <amandaft07@DESKTOP-4AD47O2.localdomain>
2022-03-30 13:01:29 -0300

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class CustomDocument extends Document {
     render() {
@@ -27,4 +28,31 @@ export default class CustomDocument extends Document {
             </Html>
         )
     }
+
+    static async getInitialProps(ctx:any) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+   
+        try {
+          ctx.renderPage = () =>
+            originalRenderPage({
+              enhanceApp: (App:any) => (props:any) =>
+                sheet.collectStyles(<App {...props} />),
+            })
+   
+          const initialProps = await Document.getInitialProps(ctx)
+          return {
+            ...initialProps,
+            styles: (
+              <>
+                {initialProps.styles}
+                {sheet.getStyleElement()}
+              </>
+            ),
+          }
+        } finally {
+          sheet.seal()
+        }
+    }
+
 }
